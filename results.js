da6f7360a95818a1c592cb9310087222dcde16a5
Raphael Torrenti <106186226+Rapsh@users.noreply.github.com>
2022-11-23 13:31:11 -0300

    Atualizar o results.js

--- results.js
+++ results.js
@@ -40,7 +40,7 @@ document.querySelector("#game-results").innerHTML =
    createCard("23/11", "quarta", 
    createGame("morocco",0,0,"croatia")+
    createGame("germany",1,2,"japan")+
-   createGame("spain",2,0,"costa-rica")+
+   createGame("spain",3,0,"costa-rica")+
    createGame("belgium",0,0,"canada")
    )+
 
